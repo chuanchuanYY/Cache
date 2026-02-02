cd78ecf817e609ce16fa26af9209a5bf685028df
huanglichuan <2012430493@qq.com>
2026-02-02 14:38:39 +0800

    添加LRUK 实现

---
+++ Cache.Core/Cache.Core/LRUKCache.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache.Core;
+
+public class LRUKCache<TValue> : ICache<TValue>
+{
+    private readonly int _k;
+    private LRUCache<int> _history;
+    private Dictionary<string, TValue> _historyKeyToValue = [];
+    private LRUCache<TValue> _cache;
+    public LRUKCache(int k,int capacity)
+    {
+        _k = k;
+        _history = new LRUCache<int>(capacity);
+        _cache  = new LRUCache<TValue>(capacity);
+    }
+    public void Add(string key, TValue value)
+    {
+        Add(key,value,TimeSpan.MaxValue);
+    }
+
+    public void Add(string key, TValue value, TimeSpan ttl)
+    {
+        
+        try
+        {
+            var history = _history.Get(key);
+            _history.Add(key, history + 1);
+            if(history + 1 >= _k)
+            {
+                _cache.Add(key, value, ttl);
+                _historyKeyToValue.Remove(key);
+                _history.Add(key, _k);
+            }
+        }
+        catch(InvalidOperationException)
+        {
+            _history.Add(key, 1);
+            _historyKeyToValue.Add(key,value);
+        }
+        
+    }
+
+    public TValue Get(string key)
+    {
+        if(TryGetValue(key,out TValue value,_cache))
+        {
+            return value;
+        }
+
+        if(TryGetValue(key,out int history,_history))
+        {
+            _history.Add(key, history + 1);
+            if (history + 1 >= _k)
+            {
+                if (_historyKeyToValue.ContainsKey(key))
+                {
+                    _cache.Add(key, _historyKeyToValue[key]);
+                    _historyKeyToValue.Remove(key);
+                    _history.Add(key, _k);
+                    return _cache.Get(key);
+                }
+                return _cache.Get(key);
+            }
+        }
+        else
+        {
+            _history.Add(key, 1);
+        }
+     
+        throw new InvalidOperationException();
+    }
+
+
+    private bool TryGetValue<T>(string key,out T value,ICache<T> cache)
+    {
+        value = default!;
+
+        try
+        {
+            value = cache.Get(key);
+            return true;
+        }
+        catch (InvalidOperationException)
+        {
+            
+            return false;
+        }
+    }
+}
---
+++ Cache.Core/Cache.Test/LRUKTest.cs
@@ -0,0 +1,45 @@
+﻿using Cache.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache.Test;
+
+internal class LRUKTest
+{
+
+    [Test]
+    public void TestK1Add()
+    {
+        var lru = new LRUKCache<int>(1, 2);
+        lru.Add("one", 1);
+        lru.Add("two", 2);
+        Assert.That(lru.Get("one"), Is.EqualTo(1));
+        Assert.That(lru.Get("two"), Is.EqualTo(2));
+    }
+
+
+    [Test]
+    public void TestK2Add()
+    {
+        var lru = new LRUKCache<int>(2, 2);
+        lru.Add("one", 1);
+        lru.Add("two", 2);
+        Assert.That(lru.Get("one"), Is.EqualTo(1));
+    }
+
+
+    [Test]
+    public void TestK3Add()
+    {
+        var lru = new LRUKCache<int>(3,10);
+        lru.Add("one", 1);
+        lru.Add("two", 2);
+        lru.Add("three", 3);
+       
+        Assert.Throws<InvalidOperationException>(() => lru.Get("one"));
+        Assert.That(lru.Get("one"), Is.EqualTo(1));
+    }
+}
